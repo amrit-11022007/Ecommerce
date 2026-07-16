32341b2ff79ba27f610cf5970195683b18f5ce8e
Amrit Raj Yadav <rajamrit4a09@gmail.com>
2026-07-17 00:05:06 +0530

    chore: Added docker

--- app/lib/database/db.ts
+++ app/lib/database/db.ts
@@ -2,10 +2,10 @@ import mysql from "mysql2/promise";
 
 // 1. Setup MySQL2 Pool (Keep this outside the request handler so it's reused)
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "aristotle",
+  database: process.env.DB_DATABASE || "ecommerce",
   waitForConnections: true,
   connectionLimit: 10,
 });
